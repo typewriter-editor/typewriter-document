6c941495d52ba68d0d1fc86c5326e291809f8389
Jacob Wright <jacwright@gmail.com>
2022-02-05 20:09:43 -0700

    Export the rest

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import AttributeMap from './delta/AttributeMap';
-import Delta from './delta/Delta';
-import Op, { OpIterator } from './delta/Op';
-export { AttributeMap, Delta, Op, OpIterator };
+export { default as AttributeMap } from './delta/AttributeMap';
+export { default as Delta } from './delta/Delta';
+export { default as Op, OpIterator } from './delta/Op';
 
 export * from './doc/EditorRange';
 export * from './doc/deltaToText';
 
-import Line, { LineIterator } from './doc/Line';
-import LineOp, { LineOpIterator } from './doc/LineOp';
-import TextChange from './doc/TextChange';
-import TextDocument from './doc/TextDocument';
-export { Line, LineIterator, LineOp, LineOpIterator, TextChange as TextChange, TextDocument as TextDocument };
+export { default as Line } from './doc/Line';
+export * from './doc/Line';
+export { default as LineOp } from './doc/LineOp';
+export * from './doc/LineOp';
+export { default as TextChange } from './doc/TextChange';
+export * from './doc/TextChange';
+export { default as TextDocument } from './doc/TextDocument';
+export * from './doc/TextDocument';
 
 export { default as cloneDeep } from './util/cloneDeep';
 export { default as isEqual } from './util/isEqual';
