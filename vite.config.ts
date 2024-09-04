c6d0ac7ecdca1b17ba5d9972e2633e1ae5c6623c
Jacob Wright <jacwright@gmail.com>
2024-09-04 09:40:58 -0600

    Re-add exports to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'Delta',
+      name: 'TypewriterDocument',
       fileName: 'index',
     },
     sourcemap: true,
